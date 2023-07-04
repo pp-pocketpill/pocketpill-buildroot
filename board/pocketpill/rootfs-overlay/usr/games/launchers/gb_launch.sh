d0fd06dba397c16b4d8ed9176edb2d2b613e9e25
olel <olel@olel.de>
2023-07-04 11:18:38 +0200

    Configured launcher to start sdl version of gnuboy

--- board/pocketpill/rootfs-overlay/usr/games/launchers/gb_launch.sh
+++ board/pocketpill/rootfs-overlay/usr/games/launchers/gb_launch.sh
@@ -2,7 +2,7 @@
 
 # Launch the process in background, record the PID into a file, wait
 # for the process to terminate and erase the recorded PID
-/usr/games/sdl2gnuboy --source /usr/games/configs/gnuboy.rc "$1"&
+env SDL_NOMOUSE=1 /usr/games/sdlgnuboy --source /usr/games/configs/gnuboy.rc "$1"&
 pid record $!
 wait $!
 pid erase
