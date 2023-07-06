b050ef1e2d530707e67753e1190d8c6c4c30627c
olel <olel@olel.de>
2023-07-06 16:10:56 +0200

    gpsp playable

--- board/pocketpill/rootfs-overlay/usr/games/launchers/gba_launch_gpsp.sh
+++ board/pocketpill/rootfs-overlay/usr/games/launchers/gba_launch_gpsp.sh
@@ -3,7 +3,8 @@
 # Launch the process in background, record the PID into a file, wait
 # for the process to terminate and erase the recorded PID
 cd ${HOME}
-gpsp "$1"&
+cp /data/gba_bios.bin ./gba_bios.bin
+env SDL_NOMOUSE=1 /usr/games/gpsp "$1"&
 pid record $!
 wait $!
 pid erase
