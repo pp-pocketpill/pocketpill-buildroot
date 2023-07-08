ad446d8131c802ae041f00bc82c24dd68b05ad1e
olel <olel@olel.de>
2023-07-08 21:06:58 +0200

    Added flashdrive

--- board/pocketpill/post-build.sh
+++ board/pocketpill/post-build.sh
@@ -2,3 +2,6 @@
 
 grep -q "GADGET_SERIAL" "${TARGET_DIR}/etc/inittab" \
 	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
+
+grep -q "/dev/mmcblk0p3" "${TARGET_DIR}/etc/fstab" \
+	|| echo '/dev/mmcblk0p3  /data           vfat    defaults         0       0' >> "${TARGET_DIR}/etc/fstab"
