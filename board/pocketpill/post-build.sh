61cd3a35b99e070622bd62649b51a3c4b04d37bb
olel <olel@olel.de>
2023-11-17 08:02:57 +0100

    switched swap/data partition order

--- board/pocketpill/post-build.sh
+++ board/pocketpill/post-build.sh
@@ -4,7 +4,7 @@ grep -q "GADGET_SERIAL" "${TARGET_DIR}/etc/inittab" \
 	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
 
 grep -q "/dev/mmcblk0p3" "${TARGET_DIR}/etc/fstab" \
-	|| echo '/dev/mmcblk0p3  /data           vfat    defaults         0       0' >> "${TARGET_DIR}/etc/fstab"
+	|| echo '/dev/mmcblk0p3  swap           swap    defaults         0       0' >> "${TARGET_DIR}/etc/fstab"
 
 grep -q "/dev/mmcblk0p4" "${TARGET_DIR}/etc/fstab" \
-	|| echo '/dev/mmcblk0p4  swap           swap    defaults         0       0' >> "${TARGET_DIR}/etc/fstab"
+	|| echo '/dev/mmcblk0p4  /data           vfat    defaults         0       0' >> "${TARGET_DIR}/etc/fstab"
